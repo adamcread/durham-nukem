82563f1135e237120f333d0259f6c75586892151
Matthew Chapman <34246991+MattChaps@users.noreply.github.com>
2020-08-29 02:12:36 +0100

    Create cathedral map

---
+++ cathedral-level/assets/tilesets/Tiny Platform Quest Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Tiny Platform Quest Tiles" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="Tiny Platform Quest Tiles.png" width="512" height="512"/>
+</tileset>
