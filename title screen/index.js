63062d4a6abee01ad796cec3078d59ed84b86a40
Matthew Chapman <34246991+MattChaps@users.noreply.github.com>
2020-08-30 14:27:22 +0100

    Add image to title screen

--- title screen/index.js
+++ title screen/index.js
@@ -3,10 +3,15 @@ export default class TitleScreen extends Phaser.Scene {
        super('TitleScreen');
    }
 
+   preload() {
+       this.load.image('title', "title screen/assets/title_image.png");
+   }
+
    create() {
+        this.add.image(20, 20, 'title');
         this.add.text(20, 20, "Loading game..."); 
         setTimeout(() => {
             this.scene.start('BillBryson')
-          }, 2000);
+          }, 5000);
    }
 }
